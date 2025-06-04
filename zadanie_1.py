0e4e9ddc2b06c3885a7dd1dbb828c176a97675b7
tomasz <tomasz@com>
2025-06-04 23:55:23 +0200

    dodanie akcji i Toolbara

--- zadanie_1.py
+++ zadanie_1.py
@@ -16,6 +16,52 @@ class MainWindow(QtWidgets.QMainWindow):
         self._createActions()
         self._createToolbar()
 
+    def _createActions(self):
+        # dodawanie ksztaltow
+
+        self.actionAddRect = QtGui.QAction("Dodaj Prostokąt", self)
+        self.actionAddRect.triggered.connect(self.addRect)
+
+        self.actionAddEllipse = QtGui.QAction("Dodaj Elipsę", self)
+        self.actionAddEllipse.triggered.connect(self.addEllipse)
+
+        self.actionAddTriangle = QtGui.QAction("Dodaj Trójkąt", self)
+        self.actionAddTriangle.triggered.connect(self.addTriangle)
+
+        # zmiana kolejności
+        self.actionBringForward = QtGui.QAction("Do przodu", self)
+        self.actionBringForward.triggered.connect(self.bringForward)
+
+
+        self.actionSendBackward = QtGui.QAction("Do tyłu", self)
+        self.actionSendBackward.triggered.connect(self.sendBackward)
+
+
+
+        # usuwanie kształtu
+        self.actionDelete = QtGui.QAction("Usuń", self)
+        self.actionDelete.triggered.connect(self.deleteItem)
+
+
+        # eksport do PNG i zapis wektorowy
+        self.actionExportPNG = QtGui.QAction("Zapisz jako PNG", self)
+        self.actionExportPNG.triggered.connect(self.exportPNG)
+        self.actionSaveVector = QtGui.QAction("Zapisz wektorowo", self)
+        self.actionSaveVector.triggered.connect(self.saveVector)
+
+    def _createToolbar(self):
+        toolbar = self.addToolBar("Narzędzia")
+        toolbar.addAction(self.actionAddRect)
+        toolbar.addAction(self.actionAddEllipse)
+        toolbar.addAction(self.actionAddTriangle)
+        toolbar.addSeparator()
+        toolbar.addAction(self.actionBringForward)
+        toolbar.addAction(self.actionSendBackward)
+        toolbar.addAction(self.actionDelete)
+        toolbar.addSeparator()
+        toolbar.addAction(self.actionExportPNG)
+        toolbar.addAction(self.actionSaveVector)
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
