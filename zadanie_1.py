2e764b2be1e8d02e47e3d6cfd09454ca1f298e6e
tomasz <tomasz@com>
2025-06-04 23:59:38 +0200

    dodawanie figur i poreacji na nich

--- zadanie_1.py
+++ zadanie_1.py
@@ -17,8 +17,8 @@ class MainWindow(QtWidgets.QMainWindow):
         self._createToolbar()
 
     def _createActions(self):
-        # dodawanie ksztaltow
 
+        # dodawanie ksztaltow
         self.actionAddRect = QtGui.QAction("Dodaj Prostokąt", self)
         self.actionAddRect.triggered.connect(self.addRect)
 
@@ -31,14 +31,11 @@ class MainWindow(QtWidgets.QMainWindow):
         # zmiana kolejności
         self.actionBringForward = QtGui.QAction("Do przodu", self)
         self.actionBringForward.triggered.connect(self.bringForward)
-
-
         self.actionSendBackward = QtGui.QAction("Do tyłu", self)
         self.actionSendBackward.triggered.connect(self.sendBackward)
 
-
-
         # usuwanie kształtu
+
         self.actionDelete = QtGui.QAction("Usuń", self)
         self.actionDelete.triggered.connect(self.deleteItem)
 
@@ -46,6 +43,7 @@ class MainWindow(QtWidgets.QMainWindow):
         # eksport do PNG i zapis wektorowy
         self.actionExportPNG = QtGui.QAction("Zapisz jako PNG", self)
         self.actionExportPNG.triggered.connect(self.exportPNG)
+
         self.actionSaveVector = QtGui.QAction("Zapisz wektorowo", self)
         self.actionSaveVector.triggered.connect(self.saveVector)
 
@@ -62,6 +60,74 @@ class MainWindow(QtWidgets.QMainWindow):
         toolbar.addAction(self.actionExportPNG)
         toolbar.addAction(self.actionSaveVector)
 
+
+
+
+    def addRect(self):     #dodaje prostokat
+        rect = QtCore.QRectF(0, 0, 100, 50)
+        item = QtWidgets.QGraphicsRectItem(rect)
+        item.setBrush(QtGui.QBrush(QtGui.QColor("lightblue")))
+        item.setPen(QtGui.QPen(QtGui.QColor("black"), 2))
+        item.setFlags(QtWidgets.QGraphicsItem.ItemIsSelectable | QtWidgets.QGraphicsItem.ItemIsMovable)
+        item.setPos(50, 50)
+        self.scene.addItem(item)
+
+
+
+    def addEllipse(self): #dodawanie elipsy
+        rect = QtCore.QRectF(0, 0, 100, 50)
+        item = QtWidgets.QGraphicsEllipseItem(rect)
+        item.setBrush(QtGui.QBrush(QtGui.QColor("lightgreen")))
+        item.setPen(QtGui.QPen(QtGui.QColor("black"), 2))
+        item.setFlags(QtWidgets.QGraphicsItem.ItemIsSelectable | QtWidgets.QGraphicsItem.ItemIsMovable)
+        item.setPos(150, 50)
+        self.scene.addItem(item)
+
+    def addTriangle(self):   #trójkąt 
+        polygon = QtGui.QPolygonF([
+            QtCore.QPointF(0, 0),
+            QtCore.QPointF(100, 0),
+            QtCore.QPointF(50, 80)
+        ])
+        item = QtWidgets.QGraphicsPolygonItem(polygon)
+        item.setBrush(QtGui.QBrush(QtGui.QColor("lightyellow")))
+        item.setPen(QtGui.QPen(QtGui.QColor("black"), 2))
+        item.setFlags(QtWidgets.QGraphicsItem.ItemIsSelectable | QtWidgets.QGraphicsItem.ItemIsMovable)
+        item.setPos(250, 50)
+        self.scene.addItem(item)
+
+    def bringForward(self):  #przesun na wierzch
+        for item in self.scene.selectedItems():
+            current_z = item.zValue()
+            item.setZValue(current_z + 1)
+
+    def sendBackward(self):  #przesun na spod
+        for item in self.scene.selectedItems():
+            current_z = item.zValue()
+            item.setZValue(current_z - 1)
+
+    def deleteItem(self):
+        for item in self.scene.selectedItems():
+            self.scene.removeItem(item)
+
+    def exportPNG(self):
+        rect = self.scene.itemsBoundingRect()
+        if rect.isNull():
+            return  
+
+
+        # Tworzenie obrazu z przezroczystym tłem
+        img = QtGui.QImage(rect.size().toSize(), QtGui.QImage.Format_ARGB32)
+        img.fill(QtCore.Qt.transparent)
+        painter = QtGui.QPainter(img)
+        self.scene.render(painter, QtCore.QRectF(img.rect()), rect)
+        painter.end()
+
+        filename, _ = QtWidgets.QFileDialog.getSaveFileName(self, "Zapisz jako PNG", "", "PNG Files (*.png)")
+        if filename:
+            img.save(filename)
+
+    
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
